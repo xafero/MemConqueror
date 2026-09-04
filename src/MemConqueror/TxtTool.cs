a9f7275b648725fb89dcac15c9203eaedb9083b0
xafero <xafero@users.noreply.github.com>
2026-09-04 20:27:19 +0200

    Adding this

--- src/MemConqueror/TxtTool.cs
+++ src/MemConqueror/TxtTool.cs
@@ -7,6 +7,9 @@ namespace MemConqueror
     {
         public static string ToStr(this Dictionary<string, object> dict)
         {
+            if (dict == null) 
+                return string.Empty;
+
             return "(" + string.Join(", ",
                 dict.Select(x => x.Key + " = " + x.Value)
             ) + ")";
---
+++ src/cleanobj.bat
@@ -0,0 +1,5 @@
+@echo off
+
+rd /s /q MemConqueror\bin
+rd /s /q MemConqueror\obj
+
