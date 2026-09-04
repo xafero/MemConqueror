63bc6a64a69457427a744b78b781be7321120fe6
xafero <xafero@users.noreply.github.com>
2026-09-04 20:51:42 +0200

    Fixing

--- src/MemConqueror/MainForm.Designer.cs
+++ src/MemConqueror/MainForm.Designer.cs
@@ -20,10 +20,10 @@ namespace MemConqueror
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.tabPage2 = new System.Windows.Forms.TabPage();
-			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.IdCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.NameCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.tabControl1.SuspendLayout();
 			this.tabPage1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -69,21 +69,6 @@ namespace MemConqueror
 			this.dataGridView1.Size = new System.Drawing.Size(756, 388);
 			this.dataGridView1.TabIndex = 0;
 			// 
-			// tabPage2
-			// 
-			this.tabPage2.Location = new System.Drawing.Point(4, 22);
-			this.tabPage2.Name = "tabPage2";
-			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(768, 400);
-			this.tabPage2.TabIndex = 1;
-			this.tabPage2.Text = "tabPage2";
-			this.tabPage2.UseVisualStyleBackColor = true;
-			// 
-			// timer1
-			// 
-			this.timer1.Interval = 500;
-			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-			// 
 			// IdCol
 			// 
 			this.IdCol.HeaderText = "Id";
@@ -93,6 +78,21 @@ namespace MemConqueror
 			// 
 			this.NameCol.HeaderText = "Name";
 			this.NameCol.Name = "NameCol";
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(768, 400);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Memory";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 500;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
 			// 
 			// MainForm
 			// 
