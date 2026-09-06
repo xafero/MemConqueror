2b5bd9192db922774d81a6be71faf95cc274f995
xafero <xafero@users.noreply.github.com>
2026-09-06 23:20:43 +0200

    Moving files

---
+++ src/MemConqueror.Lib/ProcTool.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+// ReSharper disable UseObjectOrCollectionInitializer
+
+namespace MemConqueror
+{
+    public static class ProcTool
+    {
+        public static Dictionary<string, object> GetWin32Info(this Process proc)
+        {
+            var handle = IntPtr.Zero;
+            try
+            {
+                const uint acc = Win32.PROCESS_QUERY_INFORMATION | Win32.PROCESS_VM_READ;
+                handle = Win32.OpenProcess(acc, false, proc.Id);
+                if (handle == IntPtr.Zero)
+                    return null;
+                var counters = new Win32.PROCESS_MEMORY_COUNTERS
+                {
+                    cb = Marshal.SizeOf(typeof(Win32.PROCESS_MEMORY_COUNTERS))
+                };
+                if (!Win32.GetProcessMemoryInfo(handle, out counters, counters.cb))
+                    return null;
+                var dict = new Dictionary<string, object>();
+                dict["PageFaultCount"] = counters.PageFaultCount;
+                dict["PagefileUsage"] = counters.PagefileUsage;
+                dict["PeakPagefileUsage"] = counters.PeakPagefileUsage;
+                dict["PeakWorkingSetSize"] = counters.PeakWorkingSetSize;
+                dict["PrivateUsage"] = counters.PrivateUsage;
+                dict["QuotaNonPagedPoolUsage"] = counters.QuotaNonPagedPoolUsage;
+                dict["QuotaPagedPoolUsage"] = counters.QuotaPagedPoolUsage;
+                dict["QuotaPeakNonPagedPoolUsage"] = counters.QuotaPeakNonPagedPoolUsage;
+                dict["QuotaPeakPagedPoolUsage"] = counters.QuotaPeakPagedPoolUsage;
+                dict["WorkingSetSize"] = counters.WorkingSetSize;
+                return dict;
+            }
+            catch (Exception)
+            {
+                // Nothing!
+            }
+            finally
+            {
+                Win32.CloseHandle(handle);
+            }
+            return null;
+        }
+
+        public static Dictionary<string, object> GetNetInfo(this Process proc)
+        {
+            try
+            {
+                var dict = new Dictionary<string, object>();
+                dict["Pid"] = proc.Id;
+                dict["Name"] = proc.ProcessName;
+                var pMainMod = GetMainModule(proc);
+                dict["Path"] = pMainMod.FileName;
+                dict["Desc"] = pMainMod.FileVersionInfo.FileDescription;
+                dict["VirtualMem"] = proc.VirtualMemorySize64;
+                dict["PrivateMem"] = proc.PrivateMemorySize64;
+                dict["WorkingSet"] = proc.WorkingSet64;
+                return dict;
+            }
+            catch (Exception)
+            {
+                // Nothing!
+            }
+            return null;
+        }
+
+		public static void Kill(int pid)
+		{
+            var proc = Process.GetProcessById(pid);
+            if (proc == null) return;
+            proc.Kill();
+		}
+
+        public static void Open(string file)
+        {
+            if (string.IsNullOrWhiteSpace(file)) return;
+            var info = new ProcessStartInfo { FileName = file };
+            var proc = new Process { StartInfo = info };
+            proc.Start();
+        }
+
+		private static ProcessModule GetMainModule(Process proc)
+        {
+            try
+            {
+                var main = proc.MainModule;
+                if (main.ModuleName != null)
+                    return main;
+            }
+            catch (Exception)
+            {
+                // Nothing!
+            }
+            return null;
+        }
+
+        public static Dictionary<string, object> GetWmiInfo(this Process proc)
+        {
+            try
+            {
+            	var query = string.Format("SELECT * FROM Win32_Process" +
+"            	                          WHERE ProcessId = {0}", proc.Id);
+                using (var searcher = new ManagementObjectSearcher(query))
+                    foreach (var mbo in searcher.Get())
+                    {
+                        var dict = new Dictionary<string, object>();
+                        foreach (var prop in mbo.Properties)
+                            dict[prop.Name] = prop.Value;
+                        return dict;
+                    }
+            }
+            catch (Exception)
+            {
+                // Nothing!
+            }
+            return null;
+        }
+    }
+}
---
+++ src/MemConqueror.Lib/TxtTool.cs
@@ -0,0 +1,28 @@
+using ByteSizeLib;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MemConqueror
+{
+    public static class TxtTool
+    {
+        public static string ToStr(this Dictionary<string, object> dict)
+        {
+            if (dict == null)
+                return string.Empty;
+
+            return "(" + string.Join(", ",
+                dict.Select(x => x.Key + " = " + x.Value)
+            ) + ")";
+        }
+
+        public static string ToByteSize(double value)
+        {
+            var obj = ByteSize.FromBytes(value);
+            var txt = obj.ToString();
+            if (txt == " b")
+                txt = '0' + txt;
+            return txt;
+        }
+    }
+}
