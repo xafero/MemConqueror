f1b4bb4a3bf68be8ea1a97868554945acf8e2405
xafero <xafero@users.noreply.github.com>
2026-09-09 21:30:27 +0200

    Adding json tool

---
+++ src/MemConqueror.Lib/JsonTool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MemConqueror.Lib
+{
+	public static class JsonTool
+	{
+		private static JsonSerializerSettings CreateConfig()
+		{
+			return new JsonSerializerSettings
+			{
+				Formatting = Formatting.Indented,
+				NullValueHandling = NullValueHandling.Ignore,
+				Converters = { new StringEnumConverter() }
+			};
+		}
+		
+		public static string ToJson(object obj)
+		{
+			var cfg = CreateConfig();
+			var json = JsonConvert.SerializeObject(obj, cfg);
+			return json;
+		}
+	}
+}
