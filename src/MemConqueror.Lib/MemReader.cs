65b19d23269f5379ae5a49a2bc6f2ef7e82d629b
xafero <xafero@users.noreply.github.com>
2026-09-10 23:28:29 +0200

    Adding this

---
+++ src/MemConqueror.Lib/MemReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MemConqueror.Lib
+{
+	public class MemReader : IDisposable
+	{
+		private IntPtr _handle;
+		
+		public MemReader(uint pid)
+		{
+			_handle = Win32.OpenProcess(0, false, pid);
+		}
+			
+		public void Dispose()
+		{
+			if (!Win32.CloseHandle(_handle))
+				return;
+			_handle = IntPtr.Zero;
+		}
+		
+		public bool IsDisposed { get { return _handle == IntPtr.Zero; } }
+		
+		public override string ToString()
+		{
+			return string.Format("[MemReader] handle={0:X8}", _handle);
+		}
+	}
+}
