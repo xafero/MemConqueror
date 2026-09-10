e75f4051a14444c269cdd7fe068214cdd77018a3
xafero <xafero@users.noreply.github.com>
2026-09-11 00:12:23 +0200

    Iterate now

--- src/MemConqueror.Lib/MemReader.cs
+++ src/MemConqueror.Lib/MemReader.cs
@@ -16,12 +16,23 @@ namespace MemConqueror.Lib
 			_handle = MemTool.OpenProc(_pid = pid, out _pName, rw);
 		}
 
-		public static IEnumerable<MemGot> ReadAll()
+		public IEnumerable<MemGot> ReadAll()
 		{
-			IntPtr mbiPtr = AllocMbi();
+			int mbiSize;
+			Type mbiType;
+			IntPtr mbiPtr = AllocMbi(out mbiSize, out mbiType);
 			try
 			{
-				
+				IntPtr address = IntPtr.Zero;
+				while ((int)Win32.VirtualQueryEx(_handle, address, mbiPtr, (UIntPtr)mbiSize) != 0)
+				{
+					var mbi = (MEMORY_BASIC_INFORMATION)Marshal.PtrToStructure(mbiPtr, mbiType);
+
+					long next = mbi.BaseAddress.ToInt64() + (long)mbi.RegionSize;
+					if (next <= address.ToInt64())
+						break;
+					address = new IntPtr(next);
+				}
 			}
 			finally
 			{
@@ -29,10 +40,10 @@ namespace MemConqueror.Lib
 			}
 		}
 
-		private static IntPtr AllocMbi()
+		private static IntPtr AllocMbi(out int mbiSize, out Type mbiType)
 		{
-			Type mbiType = typeof(MEMORY_BASIC_INFORMATION);
-			int mbiSize = Marshal.SizeOf(mbiType);
+			mbiType = typeof(MEMORY_BASIC_INFORMATION);
+			mbiSize = Marshal.SizeOf(mbiType);
 			return Marshal.AllocHGlobal(mbiSize);
 		}
 
