d8677d0c113a47a89efb02418ff3f8c99d772071
xafero <xafero@users.noreply.github.com>
2026-09-10 23:56:29 +0200

    Fixing

--- src/MemConqueror.Lib/MemReader.cs
+++ src/MemConqueror.Lib/MemReader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MemConqueror.Lib
 {
@@ -7,21 +8,26 @@ namespace MemConqueror.Lib
 		private uint _pid;
 		private string _pName;
 		private IntPtr _handle;
-		
+
 		public MemReader(uint pid, bool rw = false)
 		{
 			_handle = MemTool.OpenProc(_pid = pid, out _pName, rw);
 		}
-			
+
+		public static IEnumerable<MemGot> ReadAll()
+		{
+
+		}
+
 		public void Dispose()
 		{
 			if (!Win32.CloseHandle(_handle))
 				return;
 			_handle = IntPtr.Zero;
 		}
-		
+
 		public bool IsDisposed { get { return _handle == IntPtr.Zero; } }
-		
+
 		public override string ToString()
 		{
 			return string.Format("[MR] id={0}, name={1}, handle={2}", _pid, _pName, _handle);
