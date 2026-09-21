5d7d2db6b3ea86e6550b12c0f82fd641e652e40a
xafero <xafero@users.noreply.github.com>
2026-09-21 22:00:14 +0200

    Adding findy

---
+++ src/MemConqueror.Lib/FindTool.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MemConqueror.Lib
+{
+    public static class FindTool
+    {
+        public static int IndexOf(this byte[] haystack, byte[] pattern, int startIndex = 0)
+        {
+            if (pattern == null || pattern.Length == 0)
+                return -1;
+            if (haystack == null || haystack.Length < pattern.Length)
+                return -1;
+
+            var end = haystack.Length - pattern.Length;
+            for (var i = startIndex; i <= end; i++)
+            {
+                var match = true;
+                for (var j = 0; j < pattern.Length; j++)
+                {
+                    if (haystack[i + j] != pattern[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
